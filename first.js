b9e6394e8d94bcce52034f50b30d02b16d35bcd6
Looper <aayush.bhattacharjee2002@gmail.com>
2021-09-17 20:27:15 +0530

    learnt bout arrays and objs in js

--- first.js
+++ first.js
@@ -4,3 +4,14 @@ console.log(a.replace("hell","well"))
 let d = "heleelele";
 const j  ="jjello"
 console.log(j)
+let arr = [1,2,3,5,7,9,"hello",undefined,null];
+let empl = {
+    name :"name",
+    salary : 4000000,
+    hobby :"programming",
+};
+arr.push("43");
+arr[0] = 98;
+console.log(arr);
+console.log(empl);
+console.log(empl['name']);
