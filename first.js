25238b9213d27452d8cf320bf80e0ce1f05d7a28
Looper <aayush.bhattacharjee2002@gmail.com>
2021-09-18 23:24:55 +0530

    learnt funcs alert prompt confirm and loops for in,each,of

--- first.js
+++ first.js
@@ -1,17 +1,59 @@
-console.log("hello");
-var a = "hello"
-console.log(a.replace("hell","well"))
-let d = "heleelele";
-const j  ="jjello"
-console.log(j)
-let arr = [1,2,3,5,7,9,"hello",undefined,null];
-let empl = {
-    name :"name",
-    salary : 4000000,
-    hobby :"programming",
-};
-arr.push("43");
-arr[0] = 98;
-console.log(arr);
-console.log(empl);
-console.log(empl['name']);
+
+// console.log("hello");
+// var a = "hello"
+// console.log(a.replace("hell","well"))
+// let d = "heleelele";
+// const j  ="jjello"
+// console.log(j)
+// let arr = [1,2,3,5,7,9,"hello",undefined,null];
+// let empl = {
+//     name :"name",
+//     salary : 4000000,
+//     hobby :"programming",
+// };
+// arr.push("43");
+// arr[0] = 98;
+// console.log(arr);
+// console.log(empl);
+// console.log(empl['name']);
+// function sum(a=3,b=3){
+//     console.log(a+b);
+// }
+// sum();
+// alert("Hello);
+// let nam = prompt("Tell your name now","Guest");
+// console.log(nam);
+// let deletepage = confirm("Yes or no");
+// if(deletepage){
+//     console.log("Ok")
+// }
+// else{
+//     console.log("no son")
+// }
+
+// let st = "hello";
+// let age = prompt("whats your age son");
+// if(age>=18){
+//     console.log("Welcome");
+// }
+// else{
+//     alert("NO");
+// }
+// let f = ["r","h","j","k"]
+// f.forEach(function f1(element){
+//     console.log(element)
+// })
+// let emp = {
+//     name:"aayush",
+//     sal:9000000,
+//     positon:69,
+// }
+// for(ele in emp){
+//     console.log(`${ele} is ${emp[ele]}`)
+// }
+// f.forEach(element => {
+//     console.log(element);  
+// });
+// for (const iterator of f) {
+//     console.log(`${iterator} is this`)
+// }
